edd3fce684e13567a61ea05d5d74a1454ebca692
Mats Olsson <mats.olsson@callistaenterprise.se>
2013-11-08 13:10:51 +0100

    Not calling ReceiveNotificationImpl?

--- src/main/java/se/skltp/nt/intsvc/ReceiveNotificationImpl.java
+++ src/main/java/se/skltp/nt/intsvc/ReceiveNotificationImpl.java
@@ -2,9 +2,14 @@ package se.skltp.nt.intsvc;
 
 import javax.jws.WebService;
 
-import riv.infrastructure.itintegration.notification.receivenotificationresponder._1.ReceiveNotificationResponseType;
-import riv.infrastructure.itintegration.notification.receivenotificationresponder._1.ReceiveNotificationType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import se.riv.infrastructure.itintegration.notification.ReceiveNotification.v1.ReceiveNotificationResponderInterface;
+import se.riv.infrastructure.itintegration.notification.ReceiveNotificationResponder.v1.ReceiveNotificationResponseType;
+import se.riv.infrastructure.itintegration.notification.ReceiveNotificationResponder.v1.ReceiveNotificationType;
+import se.riv.infrastructure.itintegration.notification.ReceiveNotificationResponder.v1.ResultCodeEnum;
+
 
 @WebService(serviceName = "ReceiveNotificationResponderService",
         endpointInterface = "se.riv.infrastructure.itintegration.notification.ReceiveNotification.v1.ReceiveNotificationResponderInterface",
@@ -12,13 +17,22 @@ import se.riv.infrastructure.itintegration.notification.ReceiveNotification.v1.R
         targetNamespace = "urn:riv:infrastructure:itintegration:notification:ReceiveNotification:1:rivtabp21",
         wsdlLocation = "schemas/interactions/ReceiveNotificationInteraction/ReceiveNotificationInteraction_1.0_RIVTABP21.wsdl")
 public class ReceiveNotificationImpl implements ReceiveNotificationResponderInterface {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(ReceiveNotificationImpl.class);
+	
 	@Override
 	public ReceiveNotificationResponseType receiveNotification(
 			String logicalAddress,
-			ReceiveNotificationType parameters) {
-		// TODO Auto-generated method stub
-		return null;
+			ReceiveNotificationType parameters)
+	{
+		
+		log.error("##### - receiveNotification");
+		System.err.println("##### - serr receiveNotification");
+		ReceiveNotificationResponseType response = new ReceiveNotificationResponseType();
+		
+		response.setResultCode(ResultCodeEnum.OK);
+
+		return response;
 	}
 
 }
